b5b56f51eac44b61b061869459f734e28a130d9b
Joao Ricardo Cavali <joaoricardocavali@gmail.com>
2020-10-06 16:06:56 -0300

    finalized responsive styled

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,8 @@ import { Switch, Route, useLocation, useHistory, useParams } from 'react-router-
 import { ProductRegistration } from '../components';
 import Map from '../components/map';
 import { requestMenu } from '../redux/actions/get-menu';
+import UserLoginForm from '../components/user-forms/login-form';
+import UserRegForm from '../components/user-forms/registration-form';
 
 const Routes = () => {
   const location = useLocation();
@@ -23,8 +25,8 @@ const Routes = () => {
       <Route path="/product-registration">
         <ProductRegistration />
       </Route>
-      <Route path="/login">Login</Route>
-      <Route path="/register">Register</Route>
+      <Route path="/login"><UserLoginForm /></Route>
+      <Route path="/register"><UserRegForm /></Route>
       <Route path="/menu/:category/:item">Sub Menu Items</Route>
       <Route path="/menu/:category">{products && <Map type={'items'} list={products} />}</Route>
       <Route path="/menu">{categories && <Map type={'category'} list={categories} />}</Route>
