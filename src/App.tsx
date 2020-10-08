24f7f002cc4313cf5bd3a6932ab85fe4c9c5d13b
Denis Rafael Korb <drafaelk@hotmail.com>
2020-10-07 22:25:55 -0300

    request menu moved to APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { FooterMenu, TopMenu } from './components/';
+import { requestMenu } from './redux/actions/get-menu';
 import Routes from './routes';
 import './App.css';
 
 import { Container, Header, Content, Footer } from './style/app-layout';
-import { FooterMenu, TopMenu } from './components/';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(requestMenu());
+  }, []);
+
   return (
     <Container>
       <Header>
--- src/pages/category-page/index.tsx
+++ src/pages/category-page/index.tsx
@@ -1,24 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { requestMenu } from '../../redux/actions/get-menu';
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Map from '../../components/map';
 import { ContainerPages } from '../../components/styled-common';
 import { IReducer } from '../../redux/reducers';
-import { useLocation } from 'react-router-dom';
 
 const CategoryPage = () => {
-  const dispatch = useDispatch();
-  const location = useLocation();
-
-  useEffect(() => {
-    dispatch(requestMenu());
-  }, [location]);
-
-  const categories: Array<any> = useSelector((state: IReducer) => state.categories.categoriesData);
+  const categories: any[] = useSelector((state: IReducer) => state.categories.categoriesData);
 
   return (
     <ContainerPages>
-      <Map type={'category'} list={categories} />
+      <Map type="category" list={categories} />
     </ContainerPages>
   );
 };
