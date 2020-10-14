9c0c9a81860f000619d69176f079ae36d9729672
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-10-14 20:54:57 -0300

    - Badge inserted!

--- src/components/footer-menu/index.tsx
+++ src/components/footer-menu/index.tsx
@@ -3,12 +3,18 @@ import { FooterDiv, FooterButton } from './helper';
 
 import { BsFillBellFill, BsClockHistory } from 'react-icons/bs';
 import { MdRestaurantMenu } from 'react-icons/md';
+import { Badge } from 'antd';
+import { useSelector } from 'react-redux';
+import { IReducer } from '../../redux/reducers';
 
 const FooterMenu = () => {
+  const badgeCount = useSelector((state: IReducer) => state.order_list);
   return (
     <FooterDiv
       actions={[
-        <FooterButton link="order" title="Order" icon={<BsFillBellFill />} />,
+        <Badge count={badgeCount.length}>
+          <FooterButton link="order" title="Order" icon={<BsFillBellFill />} />
+        </Badge>,
         <FooterButton link="menu" title="Menu" icon={<MdRestaurantMenu />} />,
         <FooterButton link="history" title="History" icon={<BsClockHistory />} />,
       ]}
