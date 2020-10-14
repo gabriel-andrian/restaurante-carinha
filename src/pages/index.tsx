9db89c28cc4748d8ce4dae8546ae4d80948ed031
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-10-14 15:14:19 -0300

    - bug fixed in reducer history and import (pages/routers)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,6 @@ import SubCategoryPage from './sub-category-page';
 import ItemPage from './item-page';
 import OrderPage from './order-page';
 import LoginPage from './login-page';
+import KitchenPage from './kitchen-page';
 
-export { CategoryPage, SubCategoryPage, ItemPage, OrderPage, LoginPage };
+export { CategoryPage, SubCategoryPage, ItemPage, OrderPage, LoginPage, KitchenPage };
