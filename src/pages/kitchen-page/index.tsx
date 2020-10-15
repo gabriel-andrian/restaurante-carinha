6ccbad84e70bf23baae314a68811562827d02016
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2020-10-15 10:45:54 -0300

    refactor: interface of data and code cleaned

--- src/pages/kitchen-page/index.tsx
+++ src/pages/kitchen-page/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { ListToKitchen } from '../../components';
-import useSWR from 'swr';
+import useSWR, { ConfigInterface } from 'swr';
 import axios from 'axios';
 
-// essa consta abaixo DEVE ser apagada porque apenas simula o retorno da API.
+interface IData {
+  table: number;
+  orders: Array<{
+    amount: number;
+    itemId: number;
+    note: string;
+    name: string;
+    price: number;
+  }>;
+}
 
 const fetcher: (arg: string) => void = async (fetchUrl: string) => {
   const api = axios.create({
     baseURL: 'http://json-server-order-here.herokuapp.com',
     headers: { 'Content-Type': 'Application/json' },
   });
+
   const response = await api.get(fetchUrl);
 
   return response.data;
@@ -18,15 +28,15 @@ const fetcher: (arg: string) => void = async (fetchUrl: string) => {
 const KitchenPage = () => {
   const url = 'http://json-server-order-here.herokuapp.com/kitchen';
   const { data }: any = useSWR(url, fetcher, {
-    refreshInterval: 5000,
+    refreshInterval: 3000,
   });
 
   return data ? (
-    data.map(({ table, orders }: any, key: number) => (
+    data.map(({ table, orders }: IData, key: number) => (
       <ListToKitchen table={table} list={orders} key={key} />
     ))
   ) : (
-    <div>Loading</div>
+    <div>Loading kitchen orders...</div>
   );
 };
 
