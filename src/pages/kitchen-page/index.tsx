128a31849145db784ad88ec936b5273c13c39858
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2020-10-15 00:48:26 -0300

    feature: automatic reload of kitchen-page list

--- src/pages/kitchen-page/index.tsx
+++ src/pages/kitchen-page/index.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { ListToKitchen } from '../../components';
+import useSWR from 'swr';
+import axios from 'axios';
 
 // essa consta abaixo DEVE ser apagada porque apenas simula o retorno da API.
 
-const kitchen = [
-  {
-    table: 10,
-    orders: [
-      {
-        amount: 1,
-        name: 'coca',
-        note: 'sem cebola',
-      },
-      {
-        amount: 2,
-        name: 'coca',
-        note: 'com limão e gelo',
-      },
-      {
-        amount: 2,
-        name: 'coca',
-        note: '',
-      },
-    ],
-  },
-];
+const fetcher: (arg: string) => void = async (fetchUrl: string) => {
+  const api = axios.create({
+    baseURL: 'http://json-server-order-here.herokuapp.com',
+    headers: { 'Content-Type': 'Application/json' },
+  });
+  const response = await api.get(fetchUrl);
+
+  return response.data;
+};
 
 const KitchenPage = () => {
-  return kitchen.map(({ table, orders }, key) => (
-    <ListToKitchen table={table} list={orders} key={key} />
-  ));
+  const url = 'http://json-server-order-here.herokuapp.com/kitchen';
+  const { data }: any = useSWR(url, fetcher, {
+    refreshInterval: 5000,
+  });
+
+  return data ? (
+    data.map(({ table, orders }: any, key: number) => (
+      <ListToKitchen table={table} list={orders} key={key} />
+    ))
+  ) : (
+    <div>Loading</div>
+  );
 };
 
 export default KitchenPage;
