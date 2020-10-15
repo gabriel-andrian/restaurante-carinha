4c139f6c0bb4cda148da212e62418217ee9a2396
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-10-15 17:36:59 -0300

    - feat login-adm-form

---
+++ src/components/user-forms/login-adm-form/index.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { requestSession, IdataLogin } from '../../../redux/actions/login';
+import {
+  ContainerTitle,
+  H3Styled,
+  H3ErrorStyled,
+  InputEmail,
+  InputPassword,
+  ContainerSubmit,
+  InputSubmit,
+} from './styled';
+
+const UserLoginForm = () => {
+  const { register, unregister, handleSubmit, setValue, errors } = useForm();
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const submitEmail = (data: IdataLogin) => {
+    dispatch(requestSession(data));
+    history.push('/kitchen');
+  };
+
+  useEffect(() => {
+    register('email', {
+      required: 'e-mail não pode estar em branco',
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+        message: 'formato de e-mail inválido',
+      },
+    });
+    register('password', { required: 'senha não pode estar em branco' });
+    register('table', { required: 'Selecione uma mesa' });
+    return () => {
+      unregister('email');
+      unregister('password');
+    };
+  }, [register, unregister]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(submitEmail)}>
+        <ContainerTitle>
+          Seja bem vindo! <div>Faça seu login para acessar a fila dos pedidos na cozinha:</div>
+        </ContainerTitle>
+        <div>
+          {errors.email ? (
+            <H3ErrorStyled>{errors.email.message}</H3ErrorStyled>
+          ) : (
+            <H3Styled>E-mail:</H3Styled>
+          )}
+          <InputEmail onChange={(e) => setValue('email', e.target.value)} />
+        </div>
+        <div>
+          {errors.password ? (
+            <H3ErrorStyled>{errors.password.message}</H3ErrorStyled>
+          ) : (
+            <H3Styled>Senha:</H3Styled>
+          )}
+          <InputPassword onChange={(e) => setValue('password', e.target.value)} type="password" />
+        </div>
+        <ContainerSubmit>
+          <InputSubmit type="submit" value="LOGAR" />
+        </ContainerSubmit>
+      </form>
+    </>
+  );
+};
+
+export default UserLoginForm;
